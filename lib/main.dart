44821342c241960c210a7227d1cbbe4827d5bfb9
hamzakhurshid12 <hkhurshid.bese16seecs@seecs.edu.pk>
2020-12-18 01:24:57 +0500

    Test and complete User Interaction of flutter user with Android Side View

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
@@ -28,6 +30,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // This is used in the platform side to register the view.
+    final String viewType = 'platform_text_view';
+    // Pass parameters to the platform side.
+    final Map<String, dynamic> creationParams = <String, dynamic>{"camera": 1};
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -35,12 +42,29 @@ class _MyHomePageState extends State<MyHomePage> {
       body:Column(
         children: [
         Expanded(
-            child: AndroidView(
-              viewType: "platform_text_view",
-              creationParams: <String, dynamic>{"camera": 1},
-              creationParamsCodec: const StandardMessageCodec(),
-            ),
-          ),
+            child: PlatformViewLink(
+              viewType: viewType,
+              surfaceFactory:
+                  (BuildContext context, PlatformViewController controller) {
+                return AndroidViewSurface(
+                  controller: controller,
+                  gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+                  hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+                );
+              },
+              onCreatePlatformView: (PlatformViewCreationParams params) {
+                return PlatformViewsService.initSurfaceAndroidView(
+                  id: params.id,
+                  viewType: viewType,
+                  layoutDirection: TextDirection.ltr,
+                  creationParams: creationParams,
+                  creationParamsCodec: StandardMessageCodec(),
+                )
+                  ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+                  ..create();
+              },
+            )
+        ),
         ],
       ),
     );
